92223dab5e87f7ddcb0982a89db6efcfb65d8274
nahured <32559030+nahured@users.noreply.github.com>
2023-12-25 22:08:11 -0300

    dibujo.py
    estaba borracho al escribir ese código. no programar en navidad.
    
    este código funciona, es la parte que recorta las imágenes padre aumenta el tamaño y lo pega todo en un lindo collage.
    
    ahora me falta crear la parte que desarma el collage y que lo guarde en donde tendría que ir, y además si no existe las carpetas necesarias, las tiene que crear.
    
    además de verificar si agarra imágenes hermanos para así tenerlos en cuenta

--- calculadoras/dibujo.py
+++ calculadoras/dibujo.py
@@ -44,7 +44,7 @@ def padre_ref(nivel,carpeta,imagen)->tuple[tuple[float,float,float],tuple[float,
     pos_4 = False if not((carpeta1-0.5)%1==0.5 and (imagen1-0.5)%1==0) else True
 
 
-    return (int(nivel-1),int(Carpeta-1),int(Imagen-1)),(pos_1,pos_2,pos_3,pos_4)
+    return (int(nivel-1),int(Carpeta-1),int(Imagen-1)),(pos_3,pos_4,pos_1,pos_2)
 
 def crear_Carpetas(ruta) -> bool:
     if not os.path.exists(ruta):
@@ -79,55 +79,6 @@ def tileLista(l:int,x0:int,y0:int,xF:int,yF:int)->list[list]:
         listaImagenes.append(listaCarpetas)
     
     return listaImagenes
-
-
-def listaPadre(lista: list):
-    temp = []
-    elementos_encontrados = set()
-
-    if type(lista[0][0]) == tuple and type(lista[0][0][0]) == int:
-        for i in lista:
-            temp2 = []
-            for j in i:
-                padre_ref_elemento = (j[0], j[1], j[2])
-                if padre_ref_elemento not in elementos_encontrados:
-                    padreRef = padre_ref(j[0], j[1], j[2])
-                    temp2.append((j, padreRef))
-                    elementos_encontrados.add(padre_ref_elemento)
-            if len(temp2) >= 1: 
-                temp.append(temp2)
-    else:
-        for i in lista:
-            temp2 = []
-            for j in i:
-                cos = j[1][0]
-                padre_ref_elemento = (cos[0], cos[1], cos[2])
-                if padre_ref_elemento not in elementos_encontrados:
-                    padreRef = padre_ref(cos[0], cos[1], cos[2])
-                    temp2.append((j[1][0], padreRef))
-                    elementos_encontrados.add(padre_ref_elemento)
-            if len(temp2) >= 1: 
-                temp.append(temp2)
-
-    return temp
-
-
-def tiles_totales(l:int,x0:int,y0:int,xF:int,yF:int):
-    a =tileLista(l,x0,y0,xF,yF)
-    tiles:dict = {}
-    tiles[l] = a
-
-    for i in range(l,0,-1):
-        if a != False:
-            f = listaPadre(a)
-            tiles[f[0][1][1][0][0]] = f
-            a = False
-        else:
-            f = listaPadre(f)
-            tiles[f[0][1][1][0][0]] = f
-    return tiles
-
-def tiles_existentes(formato,path,diccionario:dict):
     valorTemporal:bool = True # indicamos que es el primer valor porque el primer valor se recorre de manera distinta
     existentes:dict = {}
     for i2 in diccionario:
@@ -155,7 +106,7 @@ def tiles_existentes(formato,path,diccionario:dict):
     return existentes
 
 def cortar_imagen(path,imagen,lista:list[bool,bool,bool,bool]):
-    if not imagen:
+    if imagen == False:
         img = Image.open(path)
     else:
         img = imagen
@@ -175,48 +126,41 @@ def cortar_imagen(path,imagen,lista:list[bool,bool,bool,bool]):
     img3 = img2.resize((ancho*2,alto*2))
     return img3
 
-def opterne_imagenes(formato,path,diccionario:dict):
-    imagenes:dict = {}
-    clavesTotales = list(diccionario['total'])
-    for i in clavesTotales:
-        a = diccionario['total'][i]
-        listaTemp = []
-        for j in a:
-            listaTemp2 = []
-            for k in j:
-                ruta = path + f"\\{k[0]}\\{k[1]}\\{k[2]}"+formato
-                if not os.path.exists(ruta):
-                    listaPath = []
-                    ki = k
-                    for i in range(ki[0],0,-1):
-                        listaPath.append(k)
-                        k,bol = padre_ref(k[0],k[1],k[2])
-                        if os.path.exists(path + f"\\{k[0]}\\{k[1]}\\{k[2]}"+formato):
-                            si = i
-                            listaPath.append(k)
-                            break
-                    
-                    primero = True
-                    for i in listaPath.reverse():
-                        if primero:
-                            ruta = 
-                            cortar_imagen()
-                        pass
+def obtener_imagen(path,formato,hijo):
+    padre, bol = padre_ref(hijo[0],hijo[1],hijo[2])
+    ruta = path + f"\\{padre[0]}\\{padre[1]}\\{padre[2]}"+formato
+    
+    if os.path.exists(ruta):
+        img2 = cortar_imagen(ruta,False,bol)
+        return img2
+    else:
+        imgTemp = obtener_imagen(path,formato,padre)
+        img3 = cortar_imagen(ruta,imgTemp,bol)
+        return img3
 
+def collage(path,formato,punto1,punto2,nivel,tamaño):
+    listaHijos = tileLista(nivel,punto1[0],punto1[1],punto2[0],punto2[1])
+    alto = (len(listaHijos))*tamaño
+    ancho = (len(listaHijos[0]))*tamaño
+    img = Image.new("RGB",(ancho,alto),(255,0,0))
+    for numx,x in enumerate(listaHijos):
+        for numy,y in enumerate(x):
+            imgTemp = obtener_imagen(path,formato,y)
+            img.paste(imgTemp,(tamaño*numy,tamaño*numx))
+            #img.show()
+    ruta = path + f"\\imagentemp3"+formato
+    img.save(ruta)
 
-        
-
-def armador(formato,path,l:int,x0:int,y0:int,xF:int,yF:int):
-    imagenes:dict = {}
-    imagenes["total"] = tiles_totales(l,x0,y0,xF,yF)
-    imagenes["existentes"] = tiles_existentes(formato,path,imagenes["total"])
-    imagenes["imagenes"] = opterne_imagenes(formato,path,imagenes)
-    return imagenes
 
 ruta = "D:\\programa\\cesium\\Cesium-1.112\\Build\\CesiumUnminified\\Assets\\Textures\\Mundo"
 
 formato = ".png"
-lel = armador(formato,ruta,3,4,0,2,3)
 
-#consola.clear(True)
-#print(lel)
+punto1 = (30,8)
+punto2 = (2,10)
+nivel = 4
+tamaño = 1024
+
+#print(padre_ref(3,5,5))
+
+collage(ruta,formato,punto1,punto2,nivel,tamaño)
